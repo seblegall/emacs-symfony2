fd8f0b62ef5e8dab567582afc4b13cbf5091aef3
Sébastien Le Gall <slegall@kilix.fr>
2016-01-05 09:21:23 +0100

    fix install.sh and add execution right

--- install.sh
+++ install.sh
@@ -7,11 +7,22 @@ backupFile=.emacs.backup
 while true; do
     read -p "This installation will replace your Emacs configuration. Would you like to back-up existing files? (Y/n)" yn
     case $yn in
-        [Yy]* ) mv -r $emacsDir/.emacs.d $emacsDir/$backupDir; mv $emacsDir/.emacs $emacsDir/$backupFile; break;;
-        [Nn]* ) rm $emacsDir/.emacs; rm -rf $emacsDir/.emacs.d; break;;
-        * ) echo "Please answer yes or no.";;
+        [Yy]* )
+          mv $emacsDir/.emacs.d $emacsDir/$backupDir;
+          mv $emacsDir/.emacs $emacsDir/$backupFile;
+          rm $emacsDir/.emacs;
+          rm -rf $emacsDir/.emacs.d;
+          break;;
+        [Nn]* )
+          rm $emacsDir/.emacs;
+          rm -rf $emacsDir/.emacs.d;
+          break;;
+        * )
+          echo "Please answer yes or no.";;
     esac
 done
 
-mv ../emacs-symfony2 $emacsDir/.emacs.d;
+cd ..
+mv emacs-symfony2 $emacsDir/.emacs.d;
+cd $emacsDir/.emacs.d
 printf "\e[32m Emacs for Symfony2 has been successfully installed\n";
