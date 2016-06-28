a405a95bb8785933466e686524eada9e79620944
Sébastien Le Gall <seb@le-gall.net>
2016-06-28 14:16:20 +0200

    Disable neo-tree opening when switching project using projectil.$
    This let your keep the focus on the main windows

--- init.el
+++ init.el
@@ -167,4 +167,14 @@
 
 ;; NEOTREE
 ;;--------
-(setq projectile-switch-project-action 'neotree-projectile-action)
+;;(setq projectile-switch-project-action 'neotree-projectile-action)
+(setq neo-smart-open t)
+
+;; MULTIPLE-CURSOR
+;;----------------
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
